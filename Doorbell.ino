e6312dc7db338b4bd1e67322904551d3b32b3eb9
Oops <questionable.mechanics@gmail.com>
2020-11-15 10:31:06 +0100

    Update Doorbell.ino

--- Doorbell.ino
+++ Doorbell.ino
@@ -1,15 +1,12 @@
 /*
-
-DON'T SCARE ME (sender)
-Doorbell modification and light alert system for workshed.
-
+  DON'T SCARE ME (sender)
+  Doorbell modification and light alert system for workshed.
   by: Questionable Mechanics
   contact: questionable.mechanics@gmail.com
   last rev: 10/2020
   This sketch is in the public domain
   Note: Developed and tested using VSCODE with Arduino plugin:
   https://github.com/microsoft/vscode-arduino
-
 */
 
 #include <SoftwareSerial.h>
@@ -19,11 +16,19 @@ const int bellButton = 3;  //D3
 
 void setup() {
   pinMode(bellButton, INPUT_PULLUP);
+  digitalWrite(bellButton, HIGH);
   BTSerial.begin(38400);
   Serial.begin(9600);
 }
 
 void loop() {
-  BTSerial.println(digitalRead(bellButton));
-  Serial.println(digitalRead(bellButton));
+  int buttonState = digitalRead(bellButton);
+
+  Serial.println(buttonState);
+  if (buttonState == 1) {
+    BTSerial.write('0');
+  }
+  else {
+    BTSerial.write('1');
+  }
 }
